87951600d8e11f5e81f89381c266896b3b629c2e
Kishan <Kishan@192.168.0.3>
2017-04-11 14:39:35 +0530

    1st time

---
+++ src/com/app/Employee.java
@@ -0,0 +1,21 @@
+package com.app;
+
+public class Employee {
+	
+	private int empId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + "]";
+	}
+
+	
+}
